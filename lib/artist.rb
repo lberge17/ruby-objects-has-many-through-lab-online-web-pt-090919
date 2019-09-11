3d5d7b159b0386b1c3646bcd014adb4ec630ec9e
Laura Berge <lauraeberge@gmail.com>
2019-09-11 22:27:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,17 @@ class Artist
   attr_accessor :name, :song
   @@all = []
   
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
   
+  def new_song
+    
+  end
+  
+  def self.all
+    @@all
+  end
   
 end
