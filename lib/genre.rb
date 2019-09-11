a38fed120922befa7b5f1bdee2886573e47f2d92
Laura Berge <lauraeberge@gmail.com>
2019-09-11 22:33:44 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,6 +1,19 @@
 class Genre
-  attr_accessor :song
+  attr_accessor :song, :name
   @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def songs
+    Song.all.select{|song| song.genre == self}
+  end
+  
+  def artists
+    songs.map{|song| song.artist}
+  end
   
   def self.all
     @@all
