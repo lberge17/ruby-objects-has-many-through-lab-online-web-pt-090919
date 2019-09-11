45184ae87ce9886aabfd31fc7487d34d5156ce00
Laura Berge <lauraeberge@gmail.com>
2019-09-11 22:28:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,8 +7,11 @@ class Artist
     @@all << self
   end
   
-  def new_song
-    
+  def new_song(name, genre)
+    song = Song.new
+    song.name = name
+    song.genre = genre
+    song.artist = self
   end
   
   def self.all
